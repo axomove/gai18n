cf9e6f269269f0d0fc641a8048d7f2928e7d133a
Elodie Letellier <e.letellier.dev@gmail.com>
2024-12-26 09:07:46 +0100

    upgrade ruby open ai gem version

--- lib/gai18n/configuration.rb
+++ lib/gai18n/configuration.rb
@@ -8,9 +8,6 @@ module GAI18n
     def openai_client
       args = {
         access_token: openai_secret_key,
-        extra_headers: {
-          "OpenAI-Beta" => "assistants=v2"
-        }
       }
       @openai_client ||= ::OpenAI::Client.new args
     end
